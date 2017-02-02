1c5fc0bbd13e195ab4fbd6f5c6d00813755f8bef
fsanchez2334 <fsanchez@cristoreyny.org>
2017-02-02 13:21:38 +0000

    Added Json

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-// JavaScript File$(document).ready(function(){
+$(document).ready(function(){
     $("#search").click(function() {
         //alert("This works");
  
-        var searchVal=$("#search-bar").val();
-        var first="https://api.foursquare.com/v2/venues/search?client_id=13V2HBWHG5RS5XMVAXDXD0STSY5OAVN0EQ2V0LQPBSDG34VT&client_secret=RIO145YPZWDGEJIUPLREJBQIGUO3I5UBV3UFUSNGBSCMMWKT&v=20130815&near=40.7,-74&query="
-        var url= first + searchVal;
-    $.getJSON(url,function(response){
-        
-        });
-     });
+        var searchVal1=$("#address").val();
+        var searchVal2=$("#food").val();
+        var first="https://api.foursquare.com/v2/venues/search?client_id=13V2HBWHG5RS5XMVAXDXD0STSY5OAVN0EQ2V0LQPBSDG34VT&client_secret=RIO145YPZWDGEJIUPLREJBQIGUO3I5UBV3UFUSNGBSCMMWKT&v=20130815&near="
+      
+        var url= first + searchVal1 + "&query=" + searchVal2;
+      
+       $.getJSON(url, function(response){
+           var name=response.venues.name;
+           var location=response.venues.location.formattedAddress;
+           var number=response.venues.contact.formattedPhone;
+           
+         $("body").append(name);
+         $("body").append(location);
+         $("body").append(number);
+       });
+            
 });    
+
+});
+//https://api.foursquare.com/v2/venues/search?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20130815&ll=40.7,-74&query=sushi
+ // var clientId="13V2HBWHG5RS5XMVAXDXD0STSY5OAVN0EQ2V0LQPBSDG34VT";
+//var clientSecret="RIO145YPZWDGEJIUPLREJBQIGUO3I5UBV3UFUSNGBSCMMWKT";
+ //var url= first + searchVal1 +searchVal2; 
+        //$.getJSON(url, function (response){
+            //var food
+            //var address
+            
